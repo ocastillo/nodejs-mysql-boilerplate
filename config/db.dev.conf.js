b3d0d47a88588ffb2a679c08a1b5719b84e40550
Orlando Castillo <ocastillo+league@gmail.com>
2015-05-26 22:54:21 -0500

    Updating packages to latest version and specifying specific versions

---
+++ config/db.dev.conf.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host     : 'localhost',
+    user     : 'your_username',
+    password : 'your_password',
+    database : 'your_database',
+    charset  : 'utf8'
+}
--- server.js
+++ server.js
@@ -1,4 +1,29 @@
-var express = require('express'),
+
+// TODO: Find a better way to load different configs in different env
+var dbConfig;
+try {
+    // Look for dev conf for local development
+    dbConfig = require('./config/db.dev.conf.js');
+} catch(e) {
+    try {
+        // production conf?
+        dbConfig = require('./config/db.conf.js');
+    } catch(e) {
+        console.log('Startup failed.  No db config file found.');
+        return false;
+    }
+}
+
+
+var knex = require('knex')({
+        client: 'mysql',
+        connection: dbConfig
+    }), 
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    cookieSession = require('cookie-session'),
+    serveStatic = require('serve-static'),
     expressValidator = require('express-validator'),
     flash = require('connect-flash'),
     swig = require('swig'),
@@ -9,14 +34,21 @@ var express = require('express'),
 
 var app = express();
 
-app.use(express.cookieParser());
-app.use(express.bodyParser());
+Bookshelf.mysqlAuth = Bookshelf(knex);
+
+app.use(cookieParser('halsisiHHh445JjO0'));
+app.use(cookieSession({
+    keys: ['key1', 'key2']
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(expressValidator());
-app.use(express.session({ secret: 'keyboard cat' }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
-app.use(express.static('./public'));
+app.use(serveStatic('./public'));
 //app.use(express.favicon(__dirname + '/public/images/shortcut-icon.png'));
 app.use(messages());
 
@@ -24,14 +56,9 @@ app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-// view caching
-// app.set('view cache', false);
-// swig.setDefaults({ cache: false });
-
-require('./util/bookshelf')(Bookshelf);
 require('./util/auth')(passport);
 require('./routes')(app, passport);
 
+app.listen(process.env.PORT || 3000);
 
-app.listen(3000);
 console.log('Listening on port 3000');
